123e3bf4ea8a23b43259bea1398433e34f5d6dd7
Przemurb <przemurb@hotmail.it>
2022-11-15 13:11:50 +0100

    Initial commit

---
+++ src/pl/vitoIT/baseClass/Account.java
@@ -0,0 +1,19 @@
+package pl.vitoIT.baseClass;
+
+public class Account {
+    private Long id;
+    private String login;
+    private String password;
+    private String firstname;
+    private String lastName;
+    private String nick;
+    private String email;
+    private String pesel;
+    private int age;
+    private String street;
+    private String houseNumber;
+    private String zipCode;
+    private String city;
+    private String country;
+
+}
